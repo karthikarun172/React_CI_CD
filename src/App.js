b40836aec5ad0065b408d7b3a524851a65bf065c
karthikarun172 <57855758+karthikarun172@users.noreply.github.com>
2025-01-31 19:42:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello react from arun karthik</h1>
+    </div>
+  );
+}
+
+export default App;
